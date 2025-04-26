f78c2813407fb211994639a91fc0424cc7d4684a
qianfu <qianfu46@gmain.com>
2025-04-26 11:59:09 +0800

    添加计算器应用的功能，包括引入 mathjs 库，更新按钮功能，优化样式和布局

--- calculator/script.js
+++ calculator/script.js
@@ -1,8 +1,45 @@
+import * as math from "mathjs";
+
 let display = document.getElementById("display");
 let expression = " ";
 let operator = ["+", "-", "*", "/"];
 
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".btn");
-  buttons.forEach((button) => {});
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const value = button.textContent;
+      switch (value) {
+        case "AC":
+          expression = " ";
+          display.value = expression;
+          break;
+        case "=":
+          if (expression === "Error!") expression = " ";
+          expression = cal(expression);
+          display.value = expression;
+          break;
+        case "←":
+          if (expression === "Error!") expression = " ";
+          if (expression.length) expression = expression.slice(0, -1);
+          else expression = " ";
+          display.value = expression;
+          break;
+        default:
+          if (expression === "Error!") expression = " ";
+          expression += value;
+          display.value = expression;
+          break;
+      }
+    });
+  });
 });
+
+function cal(expression) {
+  try {
+    const result = math.evaluate(expression);
+    return result.toString();
+  } catch (error) {
+    return "Error!";
+  }
+}
